decc1340b04c9f3b3cc1dfdc3c2de6700432c5d2
Alexis GESTAS <alexisgestas8@gmail.com>
2024-01-22 11:40:54 +0100

    etape 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
+#include <DHT.h>
+
+// Define the pins that we will use
+#define DHTPIN 33
+#define LED 26
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  pinMode(DHTPIN,INPUT);
+  dht.begin();
 }
 
 void loop() {
-  // put your code here to run repeatedly
+  delay(5000); // Attendre 5 secondes
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  // Afficher l'humidité et la température
+  Serial.print("Humidité: ");
+  Serial.print(humidity);
+  Serial.print(" %\t");
+  Serial.print("Température: ");
+  Serial.print(temperature);
+  Serial.println(" *C");
+
 }
